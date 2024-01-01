43891681617ff77e0c83d948105d772517c7fb15
AliAhmad810 <hiimaliahmad@gmail.com>
2023-12-31 22:08:03 -0800

    Beginning typewriter sequence

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
+const TYPE_DELAY = 60; // milliseconds
+const LINE_DELAY = TYPE_DELAY * 9; 
+const NATURAL_DELAY = TYPE_DELAY * 3;
+const NATURAL_STOPS = ",.;!?";
+
+const beginning_phrases = ["> Oh, hey there.", 
+                           "> I'm Ali, and you are?", 
+                           "> Oh, you can't speak.", 
+                           "> Well then, let me tell you a little about myself."];
+
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 const phrases = ['robotics.', 'mathematics.', 'computer science.'];
 
-const type_elem = document.getElementById("typewriter");
+const type_elem_1 = document.getElementById("typewriter_1");
 
-let sleepTime = 90; 
-
-let curPhraseIndex = 0;
-
-const writeLoop = async() => {
-    while (true) {
-        let curWord = phrases[curPhraseIndex];
-        
-        for(let i = 0; i < curWord.length; i++) {
-            type_elem.innerText = curWord.substring(0, i+1);
-            await sleep(sleepTime);
+const writeBeginningLoop = async() => {
+    let phrase = "";
+    for(let i = 0; i < beginning_phrases.length; i++) {
+        let curWord = beginning_phrases[i];
+        for(let j = 0; j < curWord.length; j++) {
+            type_elem_1.innerText = phrase + curWord.substring(0, j+1);
+            if(curWord[j] === ',') {
+                await sleep(NATURAL_DELAY);
+            }
+            await sleep(TYPE_DELAY);
         }
-        await sleep(sleepTime * 9);
+        await sleep(LINE_DELAY);
 
-        for(let i = curWord.length; i > 0; i--) {
-            type_elem.innerText = curWord.substring(0, i - 1);
-            await sleep(sleepTime * 0.9);
-        }
-        await sleep(sleepTime * 9);
-
-        curPhraseIndex = (curPhraseIndex + 1) % phrases.length;
+        phrase += curWord + "\n";
     }
 };
 
-writeLoop();
+writeBeginningLoop();
