e6bdd2ca381cd2dad38fc09bc1b8882e2ccbdd54
AliAhmad810 <hiimaliahmad@gmail.com>
2023-12-31 23:21:02 -0800

    Rewrote writeText to be reusable

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 const TYPE_DELAY = 60; // milliseconds
 const LINE_DELAY = TYPE_DELAY * 9; 
 const NATURAL_DELAY = TYPE_DELAY * 3;
-const NATURAL_STOPS = ",.;!?";
 
-const beginning_phrases = ["> Oh, hey there.", 
-                           "> I'm Ali, and you are?", 
-                           "> Oh, you can't speak.", 
-                           "> Well then, let me tell you a little about myself."];
+const text_elem = document.getElementById("typewriter_1");
+const cursor_elem = document.getElementById("cursor");
+
+var phrases = ["> Oh, hey there.", 
+               "> I'm Ali, and you are?", 
+               "> Oh, you can't speak.", 
+               "> Well then, let me tell you about myself."];
+
+var complete = true;
 
 function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const phrases = ['robotics.', 'mathematics.', 'computer science.'];
+function fadeElement(element) {
+    let op = 1; // intial opacity
+    var timer = setInterval(function() {
+        if(op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.2;
+    }, 50);
+}
 
-const type_elem_1 = document.getElementById("typewriter_1");
+function unfadeElement(element) {
+    let op = 0.1; // intial opacity
+    var timer = setInterval(function() {
+        if(op >= 0.9) {
+            clearInterval(timer);
+            element.style.display = 'inline';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.2;
+    }, 50);
+}
 
-const writeBeginningLoop = async() => {
+const writeText = async() => {
+    text_elem.innerText = "";
+    unfadeElement(text_elem);
+    unfadeElement(cursor_elem);
+    sleep(1200);
     let phrase = "";
-    for(let i = 0; i < beginning_phrases.length; i++) {
-        let curWord = beginning_phrases[i];
+    for(let i = 0; i < phrases.length; i++) {
+        let curWord = phrases[i];
         for(let j = 0; j < curWord.length; j++) {
-            type_elem_1.innerText = phrase + curWord.substring(0, j+1);
+            text_elem.innerText = phrase + curWord.substring(0, j+1);
             if(curWord[j] === ',') {
                 await sleep(NATURAL_DELAY);
             }
@@ -31,6 +61,35 @@ const writeBeginningLoop = async() => {
 
         phrase += curWord + "\n";
     }
+    fadeElement(text_elem);
+    fadeElement(cursor_elem);
+    console.log(text_elem.innerText);
 };
 
-writeBeginningLoop();
+function testFunc() {
+    text_elem.innerText = "";
+    sleep(1200);
+    let phrase = "";
+    for(let i = 0; i < phrases_1.length; i++) {
+        let curWord = phrases_1[i];
+        for(let j = 0; j < curWord.length; j++) {
+            text_elem.innerText = phrase + curWord.substring(0, j+1);
+            if(curWord[j] === ',') {
+                setTimeout(NATURAL_DELAY);
+            }
+            setTimeout(TYPE_DELAY);
+        }
+        setTimeout(LINE_DELAY);
+
+        phrase += curWord + "\n";
+    }
+
+}
+
+async function execute() {
+    writeText();
+    await sleep(11000);
+    writeText();
+}
+
+execute();
